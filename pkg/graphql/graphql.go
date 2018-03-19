65064f0642c9f08a888c7156d305389f28e3c28d
Natcha Luangaroonchai <natcha.luangaroonchai@agoda.com>
2018-03-19 13:56:00 +0700

    Remove unused codes

--- pkg/graphql/graphql.go
+++ pkg/graphql/graphql.go
@@ -58,56 +58,6 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// // Init is a function that used to register HTTP handlers to http.ServeMux object
-// func (h Handler) Init(mux *http.ServeMux) error {
-// 	schema := ggg.MustParseSchema(
-// 		string(Schema()),
-// 		h.Resolver,
-// 	)
-
-// 	mux.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
-// 		switch r.Method {
-// 		case http.MethodGet:
-// 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
-// 			w.Write(Graphiql())
-// 			return
-// 		case http.MethodPost:
-// 			var params struct {
-// 				Query         string                 `json:"query"`
-// 				OperationName string                 `json:"operationName"`
-// 				Variables     map[string]interface{} `json:"variables"`
-// 			}
-
-// 			err := json.NewDecoder(r.Body).Decode(&params)
-// 			if err != nil {
-// 				http.Error(w, err.Error(), http.StatusBadRequest)
-// 				return
-// 			}
-
-// 			response := schema.Exec(r.Context(), params.Query, params.OperationName, params.Variables)
-// 			respJSON, err := json.Marshal(response)
-// 			if err != nil {
-// 				http.Error(w, err.Error(), http.StatusInternalServerError)
-// 			}
-
-// 			w.Header().Set("Accept", "application/json")
-// 			w.Header().Set("Content-Type", "application/json")
-// 			w.Write(respJSON)
-// 			return
-// 		default:
-// 			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-// 			return
-// 		}
-// 	})
-
-// 	mux.HandleFunc("/schema.graphql", func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "application/graphql")
-// 		w.Write(Schema())
-// 	})
-
-// 	return nil
-// }
-
 // Graphiql returns a Graphiql HTML page as a byte array
 func Graphiql() []byte {
 	return generated.MustAsset("pkg/graphql/graphiql/index.html")
